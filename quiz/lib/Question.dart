2463c8d8e8234f90e67a228d3fb01e8350f06989
Akshat Garg <73712336+akshatgarg2829@users.noreply.github.com>
2023-09-29 18:06:20 +0530

    CODSOFT

--- quiz/lib/Question.dart
+++
@@ -1,7 +0,0 @@
-class Question {
-  final String questionText;
-  final List<String> options;
-  final int correctOptionIndex;
-
-  Question(this.questionText, this.options, this.correctOptionIndex);
-}
